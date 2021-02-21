4da1d9a312f441fe7b7e2db12c0b340f4271c91d
Tyler Cipriani <tcipriani@wikimedia.org>
2021-02-21 14:56:42 -0700

    Screen works
    Why this took so long to get right I have no idea

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
 #include <Arduino.h>
+#include <Adafruit_I2CDevice.h> //Included to make GFX lib happy
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
-#define LED 2
+/* -------------------- WiFi Config ----------------- */
+
+
+/* -------------------- Hardware Config ----------------- */
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+
+  // Clear the buffer.
+	display.clearDisplay();
+
+  display.begin();
+  display.clearDisplay();
+  display.setTextWrap(false);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(" Particulate Matter");
+  display.println(" scroll");
+  display.println("this screeen");
+  display.display();
+  display.startscrollright(0x00, 0x00);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
   delay(1000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+  Serial.println("FUCKKK");
 }
