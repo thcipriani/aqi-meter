fadf31d1d0cedb6d5f6eb3df79c4614ebeaa863b
Tyler Cipriani <tcipriani@wikimedia.org>
2021-07-09 12:04:42 -0600

    Builds. Update

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,8 @@
  * Much of this is work is from:
  *   - BSD License: <https://github.com/SuperHouse/AQS>
  */
-#define PROJECT "nodemcu-aqi"
+#define PROJECT "Tyler's Neat AQI Thing"
 #define VERSION "0.0.1"
-
-/**
- * Only thing in "secrets.h":
- *   - ssid
- *   - password
- *   - mqttBroker
- */
-#include "secrets.h"
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -30,7 +22,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-/* -------------------- Hardware Config ----------------- */
+/* -------------------- Hardware ----------------- */
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
@@ -42,90 +34,56 @@
 #define PMS_TX_PIN  D8
 #define PMS_BAUD  9600
 
+/**
+ * In "secrets.h"
+ *   - ssid
+ *   - password
+ *   - mqttBroker
+ */
+#include "secrets.h"
+
+const char *mqttJsonFmtString = R"JSON(
+      {
+        "PMS5003": {
+          "CF1": %i,
+          "CF2.5": %i,
+          "CF10": %i,
+          "PM1": %i,
+          "PM2.5": %i,
+          "PM10": %i,
+          "PB0.3": %i,
+          "PB0.5": %i,
+          "PB1": %i,
+          "PB2.5": %i,
+          "PB5": %i,
+          "PB10": %i
+        }
+      }",
+)JSON";
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_BME280 bme;
-Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
-Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
-Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
+Adafruit_Sensor *bmeTemp = bme.getTemperatureSensor();
+Adafruit_Sensor *bmePressure = bme.getPressureSensor();
+Adafruit_Sensor *bmeHumidity = bme.getHumiditySensor();
 
+/* -------------------- WiFi -------------------- */
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+/* -------------------- PMS5003 ----------------- */
 SoftwareSerial pmsSerial(PMS_RX_PIN, PMS_TX_PIN);
 PMS pms(pmsSerial);
 PMS::DATA pmsData;
 
-/* -------------------- WiFi Config ----------------- */
-WiFiClient espClient;
-PubSubClient client(espClient);
+enum pmsState { PMS_ASLEEP, PMS_WAKING, PMS_READY };
+pmsState pmsCurrentState;
+uint64_t pmsTimeLastRead = 0;
+uint64_t pmsWarmupTime = 30;      // seconds for warmup
+uint64_t pmsReportInterval = 120; // seconds between PMS Reads
+bool pmsReadingTaken = false;
 
-/**
- * Setup display
- * 
- * Used by the setup loop to initialize the SSD1306 OLED display
- */
-void setupDisplay() {
-  Serial.print(F("Display setup..."));
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-
-  // Clear the buffer.
-	display.clearDisplay();
-
-  display.begin();
-  display.clearDisplay();
-  display.setTextWrap(false);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(" Tyler's NEAT MONITOR!!");
-  display.display();
-  Serial.println(F("DONE!"));
-}
-
-/**
- * Used in setup loop to initialize BME280
- * 
- * Temp/pressure/humidity sensor
- */
-void setupBME() {
-  Serial.print(F("Setup BME280..."));
-
-  if (! bme.begin(BME_ADDRESS)) {
-    Serial.println("");
-    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
-    while (1) delay(10);
-  }
-
-  bme_temp->printSensorDetails();
-  bme_pressure->printSensorDetails();
-  bme_humidity->printSensorDetails();
-  Serial.println(F("DONE!"));
-}
-
-float cToF(float tempC) {
-  return (tempC * 1.8) + 32.0;
-}
-
-void render(String output) {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextWrap(false);
-  display.print(output);
-  display.display();
-}
-
-String makeBMEOutput(
-  sensors_event_t temp_event,
-  sensors_event_t pressure_event,
-  sensors_event_t humidity_event) {
-
-  char allStr[42], tempStr[15], humStr[12], pressStr[12];
-  sprintf(tempStr, "Temp: %02.02f*F", cToF(temp_event.temperature));
-  sprintf(humStr, "Hum: %02.02f%%", humidity_event.relative_humidity);
-  sprintf(pressStr, "hPa: %02.02f", pressure_event.pressure);
-  sprintf(allStr, "%s\n%s\n%s\n", tempStr, humStr, pressStr);
-   
-  return allStr;
-}
-
-bool mqttConnected() {
+void connectMQTT() {
   Serial.print("Attempting MQTT connection...");
 
   char mqttClientId[20];
@@ -150,9 +108,9 @@ bool mqttConnected() {
 
 void setupMQTT() {
   client.setServer(mqttBroker, 1883);
-  if (mqttConnected()) {
-    return;
-  }
+  client.setBufferSize(255);
+  // Could also use client.setCallback to do something when I publish a message...
+  connectMQTT();
 }
 
 void setupWifi() {
@@ -171,6 +129,147 @@ void setupWifi() {
   Serial.println("WiFi connected");
 }
 
+/**
+ * Setup display
+ * 
+ * Used by the setup loop to initialize the SSD1306 OLED display
+ */
+void setupDisplay() {
+  Serial.print(F("Display setup..."));
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+
+  // Clear the buffer.
+	display.clearDisplay();
+
+  display.begin();
+  display.clearDisplay();
+  display.setTextWrap(false);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print(" ");
+  display.print(PROJECT);
+  display.print(" ");
+  display.println(VERSION);
+  display.display();
+  Serial.println(F("DONE!"));
+}
+
+/**
+ * Used in setup loop to initialize BME280
+ * 
+ * Temp/pressure/humidity sensor
+ */
+void setupBME() {
+  Serial.print(F("Setup BME280..."));
+
+  if (! bme.begin(BME_ADDRESS)) {
+    Serial.println("");
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
+    while (1) delay(10);
+  }
+
+  bmeTemp->printSensorDetails();
+  bmePressure->printSensorDetails();
+  bmeHumidity->printSensorDetails();
+  Serial.println(F("DONE!"));
+}
+
+float cToF(float tempC) {
+  return (tempC * 1.8) + 32.0;
+}
+
+void render(String output) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextWrap(false);
+  display.print(output);
+  display.display();
+}
+
+void updateMQTT(
+  sensors_event_t tempEvent,
+  sensors_event_t pressureEvent,
+  sensors_event_t humidityEvent,
+  PMS::DATA pmsSensorData
+  ) {
+    char mqttMessageBuffer[255];
+    char mqttTopic[50];
+
+    sprintf(mqttTopic, "esp8266/%x/SENSOR", ESP.getChipId());
+    sprintf(mqttMessageBuffer, mqttJsonFmtString,
+      pmsSensorData.PM_SP_UG_2_5,            // Standard Particle calibration pm2.5 reading
+      pmsSensorData.PM_SP_UG_10_0,           // Standard Particle calibration pm10 reading
+      pmsSensorData.PM_AE_UG_1_0,            // Atmospheric Environment pm1.0 reading
+      pmsSensorData.PM_AE_UG_2_5,            // Atmospheric Environment pm2.5 reading
+      pmsSensorData.PM_AE_UG_10_0,           // Atmospheric Environment pm10.0 reading
+      pmsSensorData.PM_TOTALPARTICLES_0_3,   // Particles Per Deciliter pm0.3 reading
+      pmsSensorData.PM_TOTALPARTICLES_0_5,   // Particles Per Deciliter pm0.5 reading
+      pmsSensorData.PM_TOTALPARTICLES_1_0,   // Particles Per Deciliter pm1.0 reading
+      pmsSensorData.PM_TOTALPARTICLES_2_5,   // Particles Per Deciliter pm2.5 reading
+      pmsSensorData.PM_TOTALPARTICLES_5_0,   // Particles Per Deciliter pm5.0 reading
+      pmsSensorData.PM_TOTALPARTICLES_10_0   // Particles Per Deciliter pm10.0 reading
+    );
+
+    // post to mqtt
+    client.publish(mqttTopic, mqttMessageBuffer);
+}
+
+bool pmsTimeForReading(uint64_t timeNow) {
+  return timeNow - pmsTimeLastRead >=
+      pmsReportInterval * 1000 - pmsWarmupTime * 1000;
+}
+
+bool pmsAwake(uint64_t timeNow) {
+  return timeNow - pmsTimeLastRead >= pmsWarmupTime * 1000;
+}
+
+void readPMS() {
+  uint64_t timeNow = millis();
+  if (pmsCurrentState == PMS_ASLEEP && pmsTimeForReading(timeNow)) {
+    pms.wakeUp();
+    pmsCurrentState = PMS_WAKING;
+    pmsTimeLastRead = timeNow;
+    Serial.println("Waking PMS5003...");
+  }
+
+  if (pmsCurrentState == PMS_WAKING && pmsAwake(timeNow)) {
+    pmsCurrentState = PMS_READY;
+  }
+
+  if (pmsCurrentState == PMS_READY) {
+    pms.readUntil(pmsData);
+    pms.sleep();
+    pmsReadingTaken = true;
+    pmsCurrentState = PMS_ASLEEP;
+
+    Serial.println(pmsData.PM_AE_UG_2_5);
+  }
+}
+
+String makeBMEOutput(
+  sensors_event_t tempEvent,
+  sensors_event_t pressureEvent,
+  sensors_event_t humidityEvent) {
+
+  char allStr[42], tempStr[15], humStr[12], pressStr[12];
+  sprintf(tempStr, "Temp: %02.02f*F", cToF(tempEvent.temperature));
+  sprintf(humStr, "Hum: %02.02f%%", humidityEvent.relative_humidity);
+  sprintf(pressStr, "hPa: %02.02f", pressureEvent.pressure);
+  sprintf(allStr, "%s\n%s\n%s\n", tempStr, humStr, pressStr);
+   
+  return allStr;
+}
+
+String makePMSOutput() {
+  if (! pmsReadingTaken) {
+    return "";
+  }
+  char pmsStr[8];
+  sprintf(pmsStr, "PM2.5: %d", pmsData.PM_AE_UG_2_5);
+  return pmsStr;
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
@@ -187,20 +286,34 @@ void setup() {
 }
 
 void loop() {
-  sensors_event_t temp_event, pressure_event, humidity_event;
-  int loopCount = 0;
-  bme_temp->getEvent(&temp_event);
-  bme_pressure->getEvent(&pressure_event);
-  bme_humidity->getEvent(&humidity_event);
+  String output;
 
-  if (loopCount % 10000 == 0) {
-    pms.wakeUp();
-    pms.readUntil(pmsData);
-    pms.sleep();
+  // Reconnect wifi if needs be
+  if (WiFi.status() != WL_CONNECTED) {
+    setupWifi();
   }
 
-  Serial.println(pmsData.PM_AE_UG_2_5);
-  render(makeBMEOutput(temp_event, pressure_event, humidity_event));
+  // Reconnect mqtt if needs be
+  if (! client.connected()) {
+    connectMQTT();
+  }
+
+  // No fucking idea, copy-pasta
+  client.loop();
+
+  // Get Temp, Pressure, Humidity
+  sensors_event_t tempEvent, pressureEvent, humidityEvent;
+  bmeTemp->getEvent(&tempEvent);
+  bmePressure->getEvent(&pressureEvent);
+  bmeHumidity->getEvent(&humidityEvent);
+
+  readPMS();
+
+  output = makeBMEOutput(tempEvent, pressureEvent, humidityEvent);
+  output += makePMSOutput();
+
+  updateMQTT(tempEvent, pressureEvent, humidityEvent, pmsData);
+  render(output);
  
   delay(1000);
 }
